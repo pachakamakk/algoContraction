3b0b63e88b51a7a2a3b13dfe94a284bcf97ee211
rouchy_a <rouchy.adrien@gmail.com>
2017-01-19 12:07:54 +0100

    graph class with errors (but good file :D)

--- day1/graph.cc
+++ day1/graph.cc
@@ -1,28 +1,44 @@
 #include <utility>
 #include "graph.h"
 
-	int Graph::AddArc(int from, int to) {
-
-	}
-
-  	void Graph::AddNode(int node);
-
- 	int Graph::NumNodes() {
-		return (this.nodes.size());
- 	}
-
-  	int Graph::NumArcs() {
-		return (this.arcs.size());
-  	}
-
-  // Gets the tail ("from") and head ("to") of an arc.
-  int Graph::Tail(int arc) {
-  	
+  int Graph::AddArc(int from, int to) {
+  return (0);
   }
-  int Graph::Head(int arc) const;
 
-  // Returns a list of all the arc indices whose Tail is "from".
-  const vector<int>& Graph::OutgoingArcs(int from) const;
+    void Graph::AddNode(int node) {
+      int numNodes = this->nodes->size();
+      if (node > numNodes)
+      {
+        while(numNodes < node) {
+          this->nodes[numNodes] = new Node();
+          numNodes++;
+        }
+      }
+    }
 
-  // Returns a list of all the arc indices whose Head is "to".
-  const vector<int>& Graph::IncomingArcs(int to) const;
+  int Graph::NumNodes() const {
+    return (this->nodes->size());
+  }
+
+    int Graph::NumArcs() const {
+    return (this->arcs->size());
+    }
+
+  // Gets the tail ("from") and head ("to") of an arc->
+  int Graph::Tail(int arc) const {
+  return (this->arcs[arc]->tail);
+  }
+
+  int Graph::Head(int arc) const {
+      return (this->arcs[arc]->head);
+  }
+
+  // Returns a list of all the arc indices whose Tail is "from"->
+  vector<int>& Graph::OutgoingArcs(int from) const{
+    return (nodes[from]->outgoingArcs);
+  }
+
+  // Returns a list of all the arc indices whose Head is "to"->
+  const vector<int>& Graph::IncomingArcs(int to) const {
+    return (nodes[from]->incomingArcs);
+  }
