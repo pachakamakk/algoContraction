5fc568da01d49a8a5f00cfb4efb2b44b557e8e42
Pachakamakk <etienne.normand@epitech.eu>
2017-01-19 17:45:24 +0100

    Add map of points

--- day1/graph.h
+++ day1/graph.h
@@ -44,7 +44,7 @@ class Graph {
   // Adds a node object
   bool addNodeObj(pair<float, float>, int);
  private:
-  //  vector<pair<float, float>>
+  map<pair<float, float>, int> bufferCoord;
   vector<Node*>	nodes;
   vector<vector<int>> outgoing_arcs_;
   vector<vector<int>> incoming_arcs_;
