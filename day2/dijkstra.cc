9090a61d797ef0334c4795b0dd3d320bedb42353
Pachakamakk <etienne.normand@epitech.eu>
2017-01-20 12:45:02 +0100

    prout

--- day2/dijkstra.cc
+++ day2/dijkstra.cc
@@ -1,44 +1,44 @@
 #include "dijkstra.h"
 
-  Dijkstra(const Graph* graph, const vector<double>* arc_lengths)
+Dijkstra::Dijkstra(const Graph* graph, const vector<double>* arc_lengths)
   {
   	
   }
 
-  const Graph& GetGraph() const 
-  {
+const Graph& Dijkstra::GetGraph() const 
+{
 
-  }
+}
 
-  void RunUntilAllTargetsAreReached(int source, const vector<int>& targets) 
-  {
+void Dijkstra::RunUntilAllTargetsAreReached(int source, const vector<int>& targets) 
+{
 
-  }
+}
 
-  // Returns the set of all nodes reached by the last run.
-  const vector<int>& ReachedNodes() const
-  {
+// Returns the set of all nodes reached by the last run.
+const vector<int>& Dijkstra::ReachedNodes() const
+{
 
-  }
+}
 
-  // Element #i is the distance of node #i from the source, in the last run,
-  // which is infinity if node #i wasn't reached.
-  const vector<double>& Distances() const
-  {
+// Element #i is the distance of node #i from the source, in the last run,
+// which is infinity if node #i wasn't reached.
+const vector<double>& Dijkstra::Distances() const
+{
 
-  }
+}
 
-  // Element #i is the arc that arrives at node #i in the shortest
-  // path from the source, or -1 if that node wasn't reached. Also -1 if the
-  // node is the source.
-  const vector<int>& ParentArcs() const 
-  {
+// Element #i is the arc that arrives at node #i in the shortest
+// path from the source, or -1 if that node wasn't reached. Also -1 if the
+// node is the source.
+const vector<int>& Dijkstra::ParentArcs() const 
+{
 
-  }
+}
 
-  // Returns the full shortest path (a sequence of arcs) from the source of
-  // the last run to "node", assuming that "node" was reached.
-  vector<int> ArcPathFromSourceTo(int node) const
-  {
+// Returns the full shortest path (a sequence of arcs) from the source of
+// the last run to "node", assuming that "node" was reached.
+vector<int> Dijkstra::ArcPathFromSourceTo(int node) const
+{
 
-  }
+}
